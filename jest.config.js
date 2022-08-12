a9857b47cc1cd6ba07c325a2ec4c4ea11c259a19
Isaac Sokari <isaacsokari@gmail.com>
2022-08-12 02:29:17 +0100

    feat: use identity-obj-proxy to add classNames from css modules in test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '\\.css$': require.resolve('./test/styleMock.js')
-  }
+    '\\.module.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/styleMock.js'),
+  },
 }
--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -4,5 +4,7 @@ import {render} from '@testing-library/react'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-  render(<AutoScalingText />)
+  const {debug} = render(<AutoScalingText />)
+
+  debug()
 })
