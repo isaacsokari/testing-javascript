0b2c834eb98f6a578ebb064c3ce646f76f8bdf56
Isaac Sokari <isaacsokari@gmail.com>
2022-08-15 00:05:50 +0100

    refactor: use different settings for server and client tests

---
+++ test/jest.client.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ...require('./jest-common'),
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
+  coverageThreshold: {
+    global: {
+      statements: 15,
+      branches: 10,
+      functions: 15,
+      lines: 15,
+    },
+    // adding specific thresholds reduce the global one
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
+}
