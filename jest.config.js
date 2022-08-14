075ef78e50e61515742b9dc82f8d490a88723176
Isaac Sokari <isaacsokari@gmail.com>
2022-08-14 22:42:24 +0100

    chore: add a code coverage threshold to the repo
    - add global coverage and specific coverage

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,19 @@ module.exports = {
   snapshotSerializers: ['@emotion/jest/serializer'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 31,
+      branches: 20,
+      functions: 28,
+      lines: 31,
+    },
+    // adding specific thresholds reduce the global one
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
