221cc78b0d593b6f8a9213eeb0904d620d7b5512
Isaac Sokari <isaacsokari@gmail.com>
2022-08-17 20:53:50 +0100

    refactor: use jest-in-case and helper function to add more test cases

--- node-apps/src/utils/__tests__/auth.exercise.js
+++ node-apps/src/utils/__tests__/auth.exercise.js
@@ -1,16 +1,34 @@
 // Testing Pure Functions
 
+import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
 
-test('isPasswordAllowed returns true for valid password', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true)
-})
+function casify(obj) {
+  return Object.entries(obj).map(([name, password]) => ({
+    name: `${password} - ${name}`,
+    password,
+  }))
+}
 
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  expect(isPasswordAllowed('a2c!')).toBe(false)
-  expect(isPasswordAllowed('123456!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef!')).toBe(false)
-  expect(isPasswordAllowed('abc123!')).toBe(false)
-  expect(isPasswordAllowed('ABC123!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef123')).toBe(false)
-})
+cases(
+  'isPasswordAllowed: valid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(true)
+  },
+  casify({'valid password': '!aBc123'}),
+)
+
+cases(
+  'isPasswordAllowed: invalid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(false)
+  },
+  casify({
+    'too short': 'a2c!',
+    'no letters': '123456!',
+    'no numbers': 'ABCdef!',
+    'no uppercase letters': 'abc123!',
+    'no lowercase letters': 'ABC123!',
+    'no non-alphanumeric characters': 'ABCdef123',
+  }),
+)
