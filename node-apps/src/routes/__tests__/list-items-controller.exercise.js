02c49b22f2de3eb2f118de93267c677de67b3e7d
Isaac Sokari <isaacsokari@gmail.com>
2022-08-20 16:38:25 +0100

    test: deleteListItem and refactor file

--- node-apps/src/routes/__tests__/list-items-controller.exercise.js
+++ node-apps/src/routes/__tests__/list-items-controller.exercise.js
@@ -30,50 +30,118 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-test('getListItem returns the req.listItem', async () => {
+test(`getListItems returns a user's list items`, async () => {
   const user = buildUser()
-  const book = buildBook()
-  const listItem = buildListItem({ownerId: user.id, bookId: book.id})
 
-  // 🐨 mock booksDB.readById to resolve to the book
-  // 💰 use mockResolvedValueOnce
-  booksDB.readById.mockResolvedValueOnce(book)
+  const books = [buildBook(), buildBook()]
+  const userListItems = [
+    buildListItem({
+      ownerId: user.id,
+      bookId: books[0].id,
+    }),
+    buildListItem({
+      ownerId: user.id,
+      bookId: books[1].id,
+    }),
+  ]
+
+  booksDB.readManyById.mockResolvedValueOnce(books)
+  listItemsDB.query.mockResolvedValueOnce(userListItems)
+
+  const req = buildReq({user})
+  const res = buildRes()
+
+  await listItemsController.getListItems(req, res)
+
+  expect(listItemsDB.query).toHaveBeenCalledTimes(1)
+  expect(listItemsDB.query).toHaveBeenCalledWith({ownerId: user.id})
+
+  expect(booksDB.readManyById).toHaveBeenCalledTimes(1)
+  expect(booksDB.readManyById).toHaveBeenCalledWith([books[0].id, books[1].id])
+
+  expect(res.json).toHaveBeenCalledTimes(1)
+  expect(res.json).toHaveBeenCalledWith({
+    listItems: [
+      {...userListItems[0], book: books[0]},
+      {...userListItems[1], book: books[1]},
+    ],
+  })
+})
+
+test('deleteListItem deletes an existing list item', async () => {
+  const user = buildUser()
+  const listItem = buildListItem({ownerId: user.id})
 
   const req = buildReq({user, listItem})
   const res = buildRes()
 
-  // 🐨 make a call to getListItem with the req and res (`await` the result)
-  await listItemsController.getListItem(req, res)
-  // 🐨 assert that booksDB.readById was called correctly
-  expect(booksDB.readById).toHaveBeenCalledTimes(1)
-  expect(booksDB.readById).toHaveBeenCalledWith(book.id)
-  //🐨 assert that res.json was called correctly
+  await listItemsController.deleteListItem(req, res)
+
+  expect(listItemsDB.remove).toHaveBeenCalledWith(listItem.id)
+  expect(listItemsDB.remove).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({success: true})
   expect(res.json).toHaveBeenCalledTimes(1)
-  expect(res.json).toHaveBeenCalledWith({
-    listItem: {...listItem, book},
-  })
 })
 
-test('createListItem returns a 400 error if no bookId was passed', async () => {
-  const req = buildReq()
-  const res = buildRes()
+describe(`getListItem`, () => {
+  test('returns a specific listItem', async () => {
+    const user = buildUser()
 
-  // 🐨 make a call to createListItem with the req and res (`await` the result)
-  await listItemsController.createListItem(req, res)
-
-  // 🐨 assert that the res.status
-  expect(res.status).toHaveBeenCalledTimes(1)
-  expect(res.status).toHaveBeenCalledWith(400)
-
-  // 🐨 assert that res.json was called correctly
-  expect(res.json).toHaveBeenCalledTimes(1)
-  expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "message": "No bookId provided",
-      },
+    const books = [buildBook(), buildBook()]
+    const userListItems = [
+      buildListItem({
+        ownerId: user.id,
+        bookId: books[0].id,
+      }),
+      buildListItem({
+        ownerId: user.id,
+        bookId: books[1].id,
+      }),
     ]
-  `)
+
+    const listItem = userListItems[0]
+    const book = books[0]
+
+    booksDB.readById.mockResolvedValueOnce(book)
+
+    const req = buildReq({listItem})
+    const res = buildRes()
+
+    await listItemsController.getListItem(req, res)
+
+    expect(booksDB.readById).toHaveBeenCalledTimes(1)
+    expect(booksDB.readById).toHaveBeenCalledWith(book.id)
+
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({
+      listItem: {...listItem, book},
+    })
+  })
+
+  test('returns the req.listItem', async () => {
+    const user = buildUser()
+    const book = buildBook()
+    const listItem = buildListItem({ownerId: user.id, bookId: book.id})
+
+    // 🐨 mock booksDB.readById to resolve to the book
+    // 💰 use mockResolvedValueOnce
+    booksDB.readById.mockResolvedValueOnce(book)
+
+    const req = buildReq({user, listItem})
+    const res = buildRes()
+
+    // 🐨 make a call to getListItem with the req and res (`await` the result)
+    await listItemsController.getListItem(req, res)
+    // 🐨 assert that booksDB.readById was called correctly
+    expect(booksDB.readById).toHaveBeenCalledTimes(1)
+    expect(booksDB.readById).toHaveBeenCalledWith(book.id)
+    //🐨 assert that res.json was called correctly
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({
+      listItem: {...listItem, book},
+    })
+  })
 })
 
 describe('setListItem', () => {
@@ -159,78 +227,6 @@ describe('setListItem', () => {
   })
 })
 
-test(`getListItems returns a user's list items`, async () => {
-  const user = buildUser()
-
-  const books = [buildBook(), buildBook()]
-  const userListItems = [
-    buildListItem({
-      ownerId: user.id,
-      bookId: books[0].id,
-    }),
-    buildListItem({
-      ownerId: user.id,
-      bookId: books[1].id,
-    }),
-  ]
-
-  booksDB.readManyById.mockResolvedValueOnce(books)
-  listItemsDB.query.mockResolvedValueOnce(userListItems)
-
-  const req = buildReq({user})
-  const res = buildRes()
-
-  await listItemsController.getListItems(req, res)
-
-  expect(listItemsDB.query).toHaveBeenCalledTimes(1)
-  expect(listItemsDB.query).toHaveBeenCalledWith({ownerId: user.id})
-
-  expect(booksDB.readManyById).toHaveBeenCalledTimes(1)
-  expect(booksDB.readManyById).toHaveBeenCalledWith([books[0].id, books[1].id])
-
-  expect(res.json).toHaveBeenCalledTimes(1)
-  expect(res.json).toHaveBeenCalledWith({
-    listItems: [
-      {...userListItems[0], book: books[0]},
-      {...userListItems[1], book: books[1]},
-    ],
-  })
-})
-
-test('getListItem returns a specific listItem', async () => {
-  const user = buildUser()
-
-  const books = [buildBook(), buildBook()]
-  const userListItems = [
-    buildListItem({
-      ownerId: user.id,
-      bookId: books[0].id,
-    }),
-    buildListItem({
-      ownerId: user.id,
-      bookId: books[1].id,
-    }),
-  ]
-
-  const listItem = userListItems[0]
-  const book = books[0]
-
-  booksDB.readById.mockResolvedValueOnce(book)
-
-  const req = buildReq({listItem})
-  const res = buildRes()
-
-  await listItemsController.getListItem(req, res)
-
-  expect(booksDB.readById).toHaveBeenCalledTimes(1)
-  expect(booksDB.readById).toHaveBeenCalledWith(book.id)
-
-  expect(res.json).toHaveBeenCalledTimes(1)
-  expect(res.json).toHaveBeenCalledWith({
-    listItem: {...listItem, book},
-  })
-})
-
 describe(`createListItem`, () => {
   test('creates and returns a listItem', async () => {
     const user = buildUser()
@@ -253,23 +249,26 @@ describe(`createListItem`, () => {
     expect(res.json).toBeCalledWith({listItem: {...listItem, book}})
   })
 
-  test(`returns 400 when bookId isn't provided`, async () => {
+  test('returns a 400 error if no bookId was passed', async () => {
     const req = buildReq()
     const res = buildRes()
 
+    // 🐨 make a call to createListItem with the req and res (`await` the result)
     await listItemsController.createListItem(req, res)
 
-    expect(res.status).toBeCalledTimes(1)
-    expect(res.status).toBeCalledWith(400)
+    // 🐨 assert that the res.status
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status).toHaveBeenCalledWith(400)
 
-    expect(res.json).toBeCalledTimes(1)
+    // 🐨 assert that res.json was called correctly
+    expect(res.json).toHaveBeenCalledTimes(1)
     expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "message": "No bookId provided",
-        },
-      ]
-    `)
+    Array [
+      Object {
+        "message": "No bookId provided",
+      },
+    ]
+  `)
   })
 
   test(`returns 400 when book exists`, async () => {
